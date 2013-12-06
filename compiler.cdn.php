082791cf257584d0c5cd6c0e290146103254ae31
xiangshouding <fansekey@gmail.com>
2013-12-06 17:48:25 +0800

    update

--- compiler.cdn.php
+++ compiler.cdn.php
@@ -1,8 +1,8 @@
 <?php
 
 function smarty_compiler_cdn($params, $smarty) {
-	$cdnSync = isset($params['sync']) ? $params['sync'] : '';
-    $cdnAsync = isset($params['async']) ? $params['async'] : '';
+	$cdnSync = isset($params['sync']) ? $params['sync'] : '""';
+    $cdnAsync = isset($params['async']) ? $params['async'] : '""';
 	$strCode = '';
 	$strCode .= '<?php ';
 	$strCode .= 'if (class_exists("FISPagelet", false)) {';
